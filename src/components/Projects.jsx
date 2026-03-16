af2b9836fe3bf049040f493a4030a319c7b1e737
rahulp2321 <rahulp2321@gmail.com>
2026-03-16 08:43:55 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,24 +9,23 @@ function Projects() {
         <div className="project-card blue">
           <h3>Deploying a Web Project with Kubernetes Manifests</h3>
           <p>
-            Detailed steps for deploying a full-stack web application
+            Detailed steps for deploying a .Net core web application
             using Kubernetes YAML manifests.
           </p>
         </div>
 
         <div className="project-card green">
-          <h3>Deploying a Web Project with Helm Charts</h3>
+          <h3>Deploying a Web Project with Linux Server using Nginex</h3>
           <p>
-            Exploration of using Helm charts for simplified deployment
-            and management of Kubernetes applications.
+            Detailed steps for deploying a .Net core web application on Rocky linux .
           </p>
         </div>
 
         <div className="project-card purple">
-          <h3>Implementing a CI Workflow with GitHub Actions</h3>
+          <h3>Implementing a CICD Workflow with Jenkins and Azure DevOps</h3>
           <p>
-            Demonstration of CI pipeline using GitHub Actions for
-            automated testing and Docker builds.
+            Demonstration of CICD pipeline using Jenkins and Azure DevOps for
+            automated testing, Docker builds and Deployment at client server.
           </p>
         </div>
 
