a794b3bceda33beeef6a80200bcd230ab00ec62a
rahulp2321 <rahulp2321@gmail.com>
2026-03-16 08:35:10 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,12 +5,11 @@ function Skills() {
       <h2>Key Skills</h2>
 
       <ul>
-        <li><b>Cloud Platforms:</b> AWS (2x Certified), GCP</li>
-        <li><b>IaC & Automation:</b> Terraform (Hashicorp Certified), Ansible</li>
-        <li><b>Containerization & Orchestration:</b> Kubernetes, Docker, Helm</li>
-        <li><b>Monitoring & Observability:</b> Prometheus, Grafana, Zabbix</li>
-        <li><b>CI/CD & GitOps:</b> GitHub Actions, ArgoCD</li>
-        <li><b>Programming:</b> Python, Shell Scripting</li>
+        <li><b>Cloud Platforms:</b> AWS , GCP</li>
+        <li><b>Containerization & Orchestration:</b> Kubernetes, Docker</li>
+        <li><b>Monitoring & Observability:</b> Prometheus</li>
+        <li><b>CI/CD & GitOps:</b> Jenkins, Azure DevOps, GitLab, GitHub </li>
+        <li><b>Programming:</b> PowerShell</li>
         <li><b>Networking & Linux:</b> Expert administration</li>
       </ul>
 
